b99a799316599ba6618b79797ad6066882f9cc2d
PARAMBRATA GHOSH <138299927+param2610-cloud@users.noreply.github.com>
2024-12-08 19:45:17 +0530

    Refactor layout component and update Google Tag Manager integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,64 +6,78 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 
 const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
-    title: "Sapmate",
-    description: "Learn SAP CPI from basics to advanced with practical training & real-time projects. SAPMATE has trained 100+ students with a 99% success rate. SAPMATE - your success starts here",
+  title: "Sapmate",
+  description:
+    "Learn SAP CPI from basics to advanced with practical training & real-time projects. SAPMATE has trained 100+ students with a 99% success rate. SAPMATE - your success starts here",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <head>
-                <title>Sapmate</title>
-                <meta
-                    name="description"
-                    content="Learn SAP CPI from basics to advanced with practical training & real-time projects. SAPMATE has trained 100+ students with a 99% success rate. 
+  return (
+    <html lang="en">
+      <head>
+        <title>Sapmate</title>
+        <meta
+          name="description"
+          content="Learn SAP CPI from basics to advanced with practical training & real-time projects. SAPMATE has trained 100+ students with a 99% success rate. 
 SAPMATE - your success starts here"
-                />
-                <link rel="canonical" href="https://www.sapmate.com/" />
+        />
+        <link rel="canonical" href="https://www.sapmate.com/" />
 
-                <Script id="google-tag-manager" strategy="afterInteractive">
-                    {`
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
            })(window,document,'script','dataLayer','GTM-KZ5MWNQC');
          `}
-                </Script>
-            </head>
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <noscript>
-                    <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-KZ5MWNQC"
-                        height="0"
-                        width="0"
-                        style={{ display: "none", visibility: "hidden" }}
-                    ></iframe>
-                </noscript>
+        </Script>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16669063299"
+        ></Script>
+        <Script>
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-                {children}
-                
-                <Toaster />
-            </body>
-        </html>
-    );
+    gtag('config', 'AW-16669063299');
+  `}
+        </Script>
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KZ5MWNQC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        {children}
+
+        <Toaster />
+      </body>
+    </html>
+  );
 }
